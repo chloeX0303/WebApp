03a36d31f18aed3457da34533e768594e598fd84
chloeX0303 <ac116437@avcol.school.nz>
2024-05-06 11:48:43 +1200

    foreign department

--- WebApp/Models/Subject.cs
+++ WebApp/Models/Subject.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApp.Models
+﻿using System.Collections;
+
+namespace WebApp.Models
 {
     public class Subject
     {
         public int SubjectID { get; set; }
         public string SubjectName { get; set; }
+        public ICollection<Department> Departments { get; set; }
     }
 }
