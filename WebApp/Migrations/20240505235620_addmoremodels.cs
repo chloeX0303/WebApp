79d76ec36f675b20fd8bdc2debeca1f19988cb55
chloeX0303 <ac116437@avcol.school.nz>
2024-05-06 13:48:25 +1200

    new migration

---
+++ WebApp/Migrations/20240505235620_addmoremodels.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmoremodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubjectName",
+                table: "Subject",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentID",
+                table: "Staff",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentName",
+                table: "Department",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectID",
+                table: "Department",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_DepartmentID",
+                table: "Staff",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_SubjectID",
+                table: "Department",
+                column: "SubjectID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Subject_SubjectID",
+                table: "Department",
+                column: "SubjectID",
+                principalTable: "Subject",
+                principalColumn: "SubjectID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staff_Department_DepartmentID",
+                table: "Staff",
+                column: "DepartmentID",
+                principalTable: "Department",
+                principalColumn: "DepartmentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Subject_SubjectID",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staff_Department_DepartmentID",
+                table: "Staff");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Staff_DepartmentID",
+                table: "Staff");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Department_SubjectID",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectName",
+                table: "Subject");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentID",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentName",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectID",
+                table: "Department");
+        }
+    }
+}
