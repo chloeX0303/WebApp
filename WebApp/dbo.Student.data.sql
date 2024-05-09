c6539d8ee0a903cc53f3ea77cef7c9b6184458c3
chloeX0303 <ac116437@avcol.school.nz>
2024-05-09 15:10:14 +1200

    first student

---
+++ WebApp/dbo.Student.data.sql
@@ -0,0 +1,15 @@
+﻿SET IDENTITY_INSERT dbo.Student on
+
+INSERT INTO dbo.Student (
+	StudentID,
+	FirstName,
+	LastName,
+	Email)
+VALUES (
+	1,
+	'Kirby',
+	'Are',
+	'Kirby33@gmail.com')
+
+
+SET IDENTITY_INSERT dbo.Student off
