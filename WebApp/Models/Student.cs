4137d5da00fa8233008d9e107d276705197a2a86
chloeX0303 <ac116437@avcol.school.nz>
2024-05-10 13:43:04 +1200

    required and error mesage

--- WebApp/Models/Student.cs
+++ WebApp/Models/Student.cs
@@ -5,12 +5,15 @@ namespace WebApp.Models
     public class Student
     {
         public int StudentID { get; set; }
+        [Required(ErrorMessage = "Please enter your first name")]
         [Display(Name = "First Name")]
         [MaxLength(25)]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
         [Display(Name = "Last Name")]
         [MaxLength(25)]
         public string LastName { get; set;}
+        [Required(ErrorMessage = "Please enter your email")]
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
     }
