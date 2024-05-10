dc49505a05ec32626fc17805bd78cc62d4f1b518
chloeX0303 <ac116437@avcol.school.nz>
2024-05-10 13:54:47 +1200

    no numbers or symbols allowed!

--- WebApp/Models/Staff.cs
+++ WebApp/Models/Staff.cs
@@ -8,10 +8,14 @@ namespace WebApp.Models
         [Required(ErrorMessage = "Please enter your first name")]
         [Display(Name = "First Name")]
         [MaxLength(25)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and symbols are not allowed.")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Please enter your last name")]
         [Display(Name = "Last Name")]
         [MaxLength(25)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and symbols are not allowed.")]
         public string LastName { get; set;}
         [Required(ErrorMessage = "Please enter your email")]
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
