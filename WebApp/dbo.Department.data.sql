9717a3ac79b7424c7a6e111331898b567d017ddb
chloeX0303 <ac116437@avcol.school.nz>
2024-05-10 14:11:44 +1200

    first department

---
+++ WebApp/dbo.Department.data.sql
@@ -0,0 +1,14 @@
+﻿SET IDENTITY_INSERT dbo.Department on
+
+INSERT INTO dbo.Department (
+	DepartmentID,
+	DepartmentName,
+	SubjectID
+	)
+VALUES (
+	1,
+	'Art',
+	1
+	)
+
+SET IDENTITY_INSERT dbo.Department off
