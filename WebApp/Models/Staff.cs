bbf71695d5ed677f46952b455077dfbcf078c343
chloeX0303 <ac116437@avcol.school.nz>
2024-05-10 13:40:31 +1200

    required error message and range phone number

--- WebApp/Models/Staff.cs
+++ WebApp/Models/Staff.cs
@@ -5,16 +5,21 @@ namespace WebApp.Models
     public class Staff
     {
         public int StaffID { get; set; }
+        [Required(ErrorMessage = "Please enter your first name")]
         [Display(Name = "First Name")]
         [MaxLength(25)]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
         [Display(Name = "Last Name")]
         [MaxLength(25)]
         public string LastName { get; set;}
+        [Required(ErrorMessage = "Please enter your email")]
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
         [Display(Name = "Phone Number")]
         [MaxLength(14)]
+        [Range(0, int.MaxValue, ErrorMessage = "Numbers only")]
         public string PhoneNumber { get; set; }
     }
 }
