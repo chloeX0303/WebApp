2828dfdeaf3979060b49a20d25d2d5fcd88a4fe4
chloeX0303 <ac116437@avcol.school.nz>
2024-05-10 13:47:21 +1200

    required and error message

--- WebApp/Models/Department.cs
+++ WebApp/Models/Department.cs
@@ -5,6 +5,7 @@ namespace WebApp.Models
     public class Department
     {
         public int DepartmentID { get; set; }
+        [Required(ErrorMessage = "Please enter the department name")]
         [Display(Name = "Department Name")]
         [MaxLength(30)]
         public string DepartmentName { get; set; }
