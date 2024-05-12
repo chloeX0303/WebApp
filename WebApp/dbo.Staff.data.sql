38fa9404be8f058058b9cfbe7ca853b0e46ae296
chloeX0303 <ac116437@avcol.school.nz>
2024-05-13 11:38:19 +1200

    first staff

---
+++ WebApp/dbo.Staff.data.sql
@@ -0,0 +1,21 @@
+﻿SET IDENTITY_INSERT dbo.Staff on
+
+INSERT INTO dbo.Staff (
+	StaffID,
+	DepartmentID,
+	Email,
+	FirstName,
+	LastName,
+	PhoneNumber,
+	ProfileID
+	)
+VALUES (
+	1,
+	1,
+	'SerenaIves11@gmail.com',
+	'Serena',
+	'Ives',
+	0213499876,
+	1
+	)
+SET IDENTITY_INSERT dbo.Staff off
