4a50b79e0559134c9d3e6c4a70083e568db71a65
chloeX0303 <ac116437@avcol.school.nz>
2024-05-24 14:03:11 +1200

    data

--- WebApp/dbo.Student.data.sql
+++ WebApp/dbo.Student.data.sql
@@ -1,15 +1,3 @@
-﻿SET IDENTITY_INSERT dbo.Student on
-
-INSERT INTO dbo.Student (
-	StudentID,
-	FirstName,
-	LastName,
-	Email)
-VALUES (
-	1,
-	'Kirby',
-	'Are',
-	'Kirby33@gmail.com')
-
-
-SET IDENTITY_INSERT dbo.Student off
+﻿SET IDENTITY_INSERT [dbo].[Student] ON
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email]) VALUES (1, N'Kirby', N'Are', N'Kirby33@gmail.com')
+SET IDENTITY_INSERT [dbo].[Student] OFF
