15d4b6e7cd95d15c5ea2a9b37b9664876e056fce
chloeX0303 <ac116437@avcol.school.nz>
2024-05-30 14:51:56 +1200

    moving to a folder

---
+++ WebApp/stuff/dbo.Staff.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Staff] (
+    [StaffID]      INT            IDENTITY (1, 1) NOT NULL,
+    [DepartmentID] INT            NULL,
+    [Email]        NVARCHAR (MAX) DEFAULT (N'') NOT NULL,
+    [FirstName]    NVARCHAR (25)  NOT NULL,
+	[MidName]      NVARCHAR (25)  DEFAULT (N'') NULL,
+    [LastName]     NVARCHAR (25)  NOT NULL,
+    [PhoneNumber]  NVARCHAR (14)  NOT NULL,
+    
+    CONSTRAINT [PK_Staff] PRIMARY KEY CLUSTERED ([StaffID] ASC),
+    CONSTRAINT [FK_Staff_Department_DepartmentID] FOREIGN KEY ([DepartmentID]) REFERENCES [dbo].[Department] ([DepartmentID])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Staff_DepartmentID]
+    ON [dbo].[Staff]([DepartmentID] ASC);
+
