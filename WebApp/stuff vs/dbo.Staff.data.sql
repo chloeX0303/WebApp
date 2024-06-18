e76d6f4e242a760df9d6a4e7787441ee6c9fb109
chloeX0303 <ac116437@avcol.school.nz>
2024-06-19 10:09:02 +1200

    add data

--- WebApp/stuff vs/dbo.Staff.data.sql
+++ WebApp/stuff vs/dbo.Staff.data.sql
@@ -1,21 +1,4 @@
-﻿SET IDENTITY_INSERT dbo.Staff on
-
-INSERT INTO dbo.Staff (
-	StaffID,
-	DepartmentID,
-	Email,
-	FirstName,
-	LastName,
-	PhoneNumber,
-	ProfileID
-	)
-VALUES (
-	1,
-	1,
-	'SerenaIves11@gmail.com',
-	'Serena',
-	'Ives',
-	0213499876,
-	1
-	)
-SET IDENTITY_INSERT dbo.Staff off
+﻿SET IDENTITY_INSERT [dbo].[Staff] ON
+INSERT INTO [dbo].[Staff] ([StaffID], [DepartmentID], [Email], [FirstName], [LastName], [PhoneNumber], [MidName]) VALUES (8, NULL, N'Aven22@gmail.com', N'Aven', N'Stone', N'0212519718', NULL)
+INSERT INTO [dbo].[Staff] ([StaffID], [DepartmentID], [Email], [FirstName], [LastName], [PhoneNumber], [MidName]) VALUES (9, NULL, N'Janet300@gmail.com', N'Janet', N'Spare', N'0212519222', NULL)
+SET IDENTITY_INSERT [dbo].[Staff] OFF
