8a387182c078116c0f9a90f8282f8fb45ae60c7d
chloeX0303 <ac116437@avcol.school.nz>
2024-07-21 19:42:23 +1200

    new subject

--- WebApp/stuff vs/dbo.Subject.data.sql
+++ WebApp/stuff vs/dbo.Subject.data.sql
@@ -1,20 +1,18 @@
-﻿SET IDENTITY_INSERT dbo.Subject on
-
+﻿SET IDENTITY_INSERT [dbo].[Subject] ON
+INSERT INTO [dbo].[Subject] ([SubjectID], [SubjectName], [DepartmentID], [ImageName]) VALUES (1, N'Art', NULL, N'')
+INSERT INTO [dbo].[Subject] ([SubjectID], [SubjectName], [DepartmentID], [ImageName]) VALUES (2, N'Health PhysicalEducationsss', NULL, N'')
+INSERT INTO [dbo].[Subject] ([SubjectID], [SubjectName], [DepartmentID], [ImageName]) VALUES (3, N'Science', NULL, N'hans-reniers-lQGJCMY5qcM-unsplash2432235242.jpg')
+INSERT INTO [dbo].[Subject] ([SubjectID], [SubjectName], [DepartmentID], [ImageName]) VALUES (4, N'English', NULL, N'laura-ohlman-qRtcb9wHG40-unsplash2439300765.jpg')
+INSERT INTO [dbo].[Subject] ([SubjectID], [SubjectName], [DepartmentID], [ImageName]) VALUES (5, N'Mathematics', NULL, N'clayton-robbins-ihqB-c8C7Bc-unsplash2440031296.jpg')
 INSERT INTO dbo.Subject (
 	SubjectID,
-	SubjectName
-	)
+	SubjectName,
+	DepartmentID,
+	ImageName)
 VALUES (
-	1,
-	'Art'
-	)
-INSERT INTO dbo.Subject (
-	SubjectID,
-	SubjectName
-	)
-VALUES (
+	6,
+	'Social Science',
 	2,
-	'Commerce'
-	)
+	'social-science.jpg')
 
-SET IDENTITY_INSERT	dbo.Subject off
+SET IDENTITY_INSERT [dbo].[Subject] OFF
